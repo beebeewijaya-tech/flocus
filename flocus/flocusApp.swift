ffd2cd009ff82622095994187daa7cd78cefe5a4
Bee <beebeewijaya.tech@gmail.com>
2026-04-06 16:16:02 +0700

    Initial Commit

---
+++ flocus/flocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  flocusApp.swift
+//  flocus
+//
+//  Created by Bee Wijaya on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct flocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
