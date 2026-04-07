8406df239d33453c1e919b1d5c5180370161d142
Bee <beebeewijaya.tech@gmail.com>
2026-04-07 23:09:41 +0700

    chore: separate and reusing fullmodal component
    separating the fullmodal and supporting reusable options
    by allowing to hide and show the close button
    
    due to very looks like UI in terms of layout, colors, button position
    would be better to just use 1 fullmodal and passed variable from there

--- flocus/Screen/Modal1Screen.swift
+++ flocus/Screen/Modal1Screen.swift
@@ -13,69 +13,54 @@ struct Modal1Screen: View {
     @State private var selected = 15
     
     var body: some View {
-        ZStack {
-            Color("Secondary")
-                .ignoresSafeArea()
+        FullModal(content: {
+            VStack(spacing: 4) {
+                Text("Current Task:")
+                    .foregroundColor(Color("Primary"))
+                Text("PR Sekolah")
+                    .font(.title.bold())
+                    .padding(10)
+                    .foregroundColor(Color("Primary"))
+            }
+            .padding(.top,40)
             
-            VStack (spacing: 16){
-                
-                HStack {
-                    
-                    Button(action: {showed = false}) {
-                        Image(systemName: "xmark")
-                            .padding()
-                            .glassEffect()
-                            .foregroundColor(Color("Primary"))
+            
+            Text("Set your duration")
+                .padding(.top,50)
+                .foregroundColor(Color("Primary"))
+            
+            HStack {
+                Picker("Min", selection: $selected) {
+                    ForEach(15...45, id: \.self) { minute in
+                        Text("\(minute)").tag(minute)
                     }
-                    Spacer()
                 }
-                .padding(.horizontal)
+                .pickerStyle(.wheel)
+                .frame(width:120)
                 
-                
-                VStack(spacing: 4) {
-                    Text("Current Task:")
-                        .foregroundColor(Color("Primary"))
-                    Text("PR Sekolah")
-                        .font(.title.bold())
-                        .padding(10)
-                        .foregroundColor(Color("Primary"))
-                }
-                .padding(.top,40)
-                
-                
-                Text("Set your duration")
-                    .padding(.top,50)
+                Text("Min")
+                    .font(.headline)
                     .foregroundColor(Color("Primary"))
                 
-                HStack {
-                    Picker("Min", selection: $selected) {
-                        ForEach(15...45, id: \.self) { minute in
-                            Text("\(minute)").tag(minute)
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width:120)
-                    
-                    Text("Min")
-                        .font(.headline)
-                        .foregroundColor(Color("Primary"))
-                    
-                    
-                }
-                Spacer()
                 
-                Button(action: {
-                    print("Durasi dipilih: \(selected) menit")
-                    showed = false}) {
-                        Text("Start")
-                            .frame(width: 120,height: 35)
-                            .padding()
-                            .background(Color("Primary"))
-                            .foregroundColor(.white)
-                            .clipShape(Capsule())
-                    }
-                    .padding(.bottom,120)
             }
-        }
+            Spacer()
+            
+            Button(action: {
+                print("Durasi dipilih: \(selected) menit")
+                showed = false}) {
+                    Text("Start")
+                        .frame(width: 120,height: 35)
+                        .padding()
+                        .background(Color("Primary"))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+                .padding(.bottom,120)
+        }, showed: $showed)
     }
+}
+
+#Preview {
+    Modal1Screen(showed: .constant(true))
 }
